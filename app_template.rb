ab7bdd00fd0a4cee83ee4f79f60c29dea4415c41
shu0115 <s.matsumoto0115@gmail.com>
2012-12-22 18:29:09 +0900

    Update

--- app_template.rb
+++ app_template.rb
@@ -29,8 +29,8 @@ gem 'rainbow'
 # Dubug Tool
 gem 'tapp'
 
-# Deployment
-gem_group :deployment do
+# Development
+gem_group :development do
   # Twitter Bootstrap
   gem 'twitter-bootstrap-scaffold'
 
@@ -38,10 +38,14 @@ gem_group :deployment do
   gem 'sextant'
 end
 
-# Test AND Deployment
-gem_group :test, :development do
-  gem "rspec-rails"
+# Test AND Development
+gem_group :development, :test do
   gem 'sqlite3'
+end
+
+# Test
+gem_group :test do
+  gem 'rspec-rails'
 end
 
 # Production
@@ -50,14 +54,69 @@ gem_group :production do
   gem 'newrelic_rpm'
 end
 
-# CommentOut
-comment_lines 'Gemfile', "gem 'sqlite3'"
-comment_lines 'Gemfile', "gem 'action_args'"
-
+#--------------#
+# Insert Files #
+#--------------#
 # Ruby Version Insert
 insert_into_file "Gemfile",
-  "source \"https://rubygems.org\"\n",
-  after: "ruby \"1.9.3\"\n"
+  "ruby '1.9.3'\n",
+  after: "source 'https://rubygems.org'\n"
+
+insert_into_file "Gemfile",
+  "# OmniAuth\n",
+  before: "gem \"omniauth-scaffold\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Scaffold Template\n",
+  before: "gem \"custom-template\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Markdown\n",
+  before: "gem \"markdown-scaffold\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Utility\n",
+  before: "gem \"action_args\"\n"
+
+insert_into_file "Gemfile",
+  "\n# App Server\n",
+  before: "gem \"thin\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Pagination\n",
+  before: "gem \"kaminari\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Settings\n",
+  before: "gem \"rails_config\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Colorful Logging\n",
+  before: "gem \"rainbow\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Dubug Tool\n",
+  before: "gem \"tapp\"\n"
+
+insert_into_file "Gemfile",
+  "\n# Development\n",
+  before: "group :development do\n"
+
+insert_into_file "Gemfile",
+  "# Test AND Development\n",
+  before: "gem_group :development, :test do\n"
+
+insert_into_file "Gemfile",
+  "# Test\n",
+  before: "group :test do\n"
+
+insert_into_file "Gemfile",
+  "# Production\n",
+  before: "group :production do\n"
+
+# CommentOut
+comment_lines 'Gemfile', "gem 'sqlite3'"
+comment_lines 'Gemfile', "gem \"action_args\""
 
 # Gem Install
 run "bundle install --without production"
