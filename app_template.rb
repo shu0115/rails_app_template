af8ab78ec87516abc6f13e4b8253f3d5afed4c27
shu0115 <s.matsumoto0115@gmail.com>
2012-12-22 18:01:06 +0900

    コメントアウト削除

--- app_template.rb
+++ app_template.rb
@@ -2,36 +2,6 @@
 # Application Template #
 #----------------------#
 
-=begin
-repo_url = "https://raw.github.com/tachiba/rails3_template/master"
-gems = {}
-
-@app_name = app_name
-
-def get_and_gsub(source_path, local_path)
-  get source_path, local_path
-
-  gsub_file local_path, /%app_name%/, @app_name
-  gsub_file local_path, /%app_name_classify%/, @app_name.classify
-  gsub_file local_path, /%working_user%/, @working_user
-  gsub_file local_path, /%working_dir%/, @working_dir
-  #gsub_file local_path, /%remote_repo%/, @remote_repo if @remote_repo
-end
-
-def gsub_database(localpath)
-  return unless @mysql
-
-  gsub_file localpath, /%database_name%/, @mysql[:database_name] ? @mysql[:database_name] : @app_name
-
-  gsub_file localpath, /%mysql_username_development%/, @mysql[:username_development]
-  gsub_file localpath, /%mysql_remote_host_development%/, @mysql[:remote_host_development]
-
-  gsub_file localpath, /%mysql_username_production%/, @mysql[:username_production]
-  gsub_file localpath, /%mysql_password_production%/, @mysql[:password_production]
-  gsub_file localpath, /%mysql_remote_host_production%/, @mysql[:remote_host_production]
-end
-=end
-
 # OmniAuth
 gem 'omniauth-scaffold'
 
